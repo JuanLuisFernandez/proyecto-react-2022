343e0866a49d4d683e56297f9bf56121299baa1a
Juan Luis Fernandez <juan.luis.fernandez@maexware-solutions.de>
2022-10-04 12:53:19 +0200

    Login und CSS del login

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useEffect, useState } from "react";
+
+const UserContext = createContext()
+
+const UserContextProvider = ({ children }) => {
+
+    const [usuario, setUsuario] = useState({})
+
+    useEffect(() => {
+        setUsuario({
+            name: "Juanito Perez",
+            registered: "15/Agosto/2022"
+        })
+    }, [])
+
+    return (
+        <UserContext.Provider value={usuario} >
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export { UserContext, UserContextProvider }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./components/App";
+import Home from "./Home"
+import Perfil from "./components/usuarios/Perfil"
+import Login from "./components/usuarios/Login"
+import Cuadricula from "./components/Cuadricula"
+import CriptoPage from "./components/cripto/CriptoPage"
+import "./main.css";
+import Pagina404 from "./components/404";
+import { UserContextProvider } from "./context/UserContext"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+        <UserContextProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />}>
+                        <Route index element={<Home />} />
+                        <Route path='perfil' element={<Perfil />} />
+                    </Route>
+                    <Route path="/criptomonedas" element={<App />}>
+                        <Route index element={<Cuadricula />} />
+                        <Route path=':id' element={<CriptoPage />} />
+                    </Route>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<Pagina404 />} />
+                </Routes>
+            </BrowserRouter>
+        </UserContextProvider>
 )
